2822bfd0f7317c12d4f19eedb956d32474710e49
codingdie <547912355@qq.com>
2017-04-21 19:08:08 +0800

    update

---
+++ src/main/java/com/codingdie/tiebaspider/model/PostSimpleInfo.java
@@ -0,0 +1,17 @@
+package com.codingdie.tiebaspider.model;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Created by xupeng on 2017/4/19.
+ */
+public class PostSimpleInfo {
+    public String lastUpdateTime;
+    public String lastUpdateUser;
+    public String createUser;
+    public String title;
+    public int remarkNum ;
+    public String postId ;
+    public String type="normal" ;
+
+}
